8e1e417c85c896a1464d11f6e20f51eb1c51b29c
Evgeny <mrvyala@gmail.com>
2026-02-17 00:52:10 +0300

    Add package myApp

---
+++ src/l2/robot/Factory.java
@@ -0,0 +1,24 @@
+package l2.robot;
+
+
+public class Factory implements Runnable {
+
+    private final Dump dump;
+
+    public Factory(Dump dump) {
+        this.dump = dump;
+    }
+
+    @Override
+    public void run() {
+
+        for (int night = 0; night < Constants.COUNT_OF_NIGHTS_IN_COMPETITION; night++) {
+            int count = Utils.getRandomCountOfDetails();
+
+            for (int i = 0; i < count; i++) {
+                dump.add(Utils.getRandomDetail());
+            }
+            Utils.sleepNight();
+        }
+    }
+}
